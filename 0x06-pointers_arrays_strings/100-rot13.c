d6a9c8f7fe8b70b98b3533b72cde7d1a1ff41dea
Hlina21 <hlina.nova@gmail.com>
2022-12-22 09:05:28 -0800

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,31 +9,23 @@
 
 char *rot13(char *str);
 {
-	int index1;
-	int index2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-		'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-		'u', 'v', 'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C',
-			'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-			'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-			'h', 'i', 'j', 'k', 'l', 'm'};
+	int index1 = 0, index2 = 0;
+	char string_rot13[] = "NnOnPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm"
+	char string_aplha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
 
-	for (index1 = 0; str[index1] != '\0')
+	while (str[index1] != '\0')
 	{
-		for (index2 = 0; index2 < 52; index2++)
-		{
-			if (str[index1] == alphabet[index2])
+		do {
+			if (str[index1] == string_alpha[index2])
 			{
-				str[index1] = rot13key[index2];
+				str[index1] = string_alpha[index2];
 				break;
 			}
+			index2++;
 		}
+	while (string_alpha[index2] != '\0')
+		index2 = 0;
+	index1++;
 	}
 	return (str);
 }
