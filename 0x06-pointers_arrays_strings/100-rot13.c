54157b00f8ef6e5279304aea4f792a14548bd62f
Hlina21 <hlina.nova@gmail.com>
2022-12-22 08:44:20 -0800

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,13 +4,13 @@
  * rot13 - encodes a string using rot13.
  * @str: the string to be encoded.
  *
- * Return: a pointer to the encoded string.
+ * Return: string.
  */
 
 char *rot13(char *str);
 {
-	int index1 = 0, index2;
-
+	int index1;
+	int index2;
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 		'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
 		'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -23,7 +23,8 @@ char *rot13(char *str);
 			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
 			'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
 			'h', 'i', 'j', 'k', 'l', 'm'};
-	while (str[index1])
+
+	for (index1 = 0; str[index1] != '\0')
 	{
 		for (index2 = 0; index2 < 52; index2++)
 		{
@@ -33,7 +34,7 @@ char *rot13(char *str);
 				break;
 			}
 		}
-		index1++;
 	}
 	return (str);
+}
 
