0c30d5ac3a59e477309bc25ef2980ddfdddc30e9
Hlina21 <hlina.nova@gmail.com>
2022-12-22 23:36:45 -0800

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Add one line
- * followed by a new line
  * Return: Always 0
  */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+	int n, int a[5], int *p;
 
 	a[2] = 1024;
 	p = &n;
