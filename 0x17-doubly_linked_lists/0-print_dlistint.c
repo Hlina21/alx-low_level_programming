610f77ce8381a2e0bc4d66d26b227ca9ac4a1a91
Hlina21 <hlina.nova@gmail.com>
2023-03-16 05:02:06 -0700

    all task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,26 +1,22 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a
- * dlistint_t list
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: The head of the dlistint_t list.
  *
- * @h: head of the list
- * Return: the number of nodes
+ * Return: The number of nodes in the list.
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+	size_t i = 0;
+	const dlistint_t *tmp = h;
 
-	count = 0;
-	if (h == NULL)
-		return (count);
-	while (h->prev != NULL)
-		h = h->prev;
-	while (h != NULL)
+	while (tmp != NULL)
 	{
-		printf("%d\n", h->n);
-		count++;
-		h = h->next;
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		i++;
 	}
-	return (count);
+	return (i);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,17 +1,15 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LIST_H
+#define LIST_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
- *
  * Description: doubly linked list node structure
- * for Holberton project
  */
 typedef struct dlistint_s
 {
@@ -29,5 +27,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
 #endif
