982a427fcbf1074ff8787e339ffe177a5e5525ef
Hlina21 <hlina.nova@gmail.com>
2023-03-17 09:48:55 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
+	size_t j = 0;
 	const dlistint_t *tmp = h;
 
 	while (tmp != NULL)
 	{
 		printf("%d\n", tmp->n);
 		tmp = tmp->next;
-		i++;
+		j++;
 	}
-	return (i);
+	return (j);
 }
